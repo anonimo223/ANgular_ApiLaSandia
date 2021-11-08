b3193a3d7003ee8a8f7daa2ea00f4c15ef1b5762
anonimo223 <anguielugo44@gmail.com>
2021-11-07 22:11:59 -0500

    lugo: Carga de todo el paso a paso del prime proyecto con Angular, ignora uno de los archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SandiasComponent } from './sandias/sandias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SandiasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule //Se importa FormsModule.
   ],
   providers: [],
   bootstrap: [AppComponent]
